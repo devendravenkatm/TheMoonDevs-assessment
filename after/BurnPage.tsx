17d7d57957a8a251bfe18be9a7500223ecc61273
Devendra Venkat Mutyala <88088773+devendravenkatm@users.noreply.github.com>
2024-02-16 17:26:04 +0530

    update

--- after/BurnPage.tsx
+++ after/BurnPage.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import BurnButtonBar from './BurnButtonBar';
-import BurnStatsContainer from './BurnStatsContainer';
-import BurnTxTable from './BurnTxTable';
-import ChainSelector from './ChainSelector';
-import AppToast from './AppToast';
+import BurnButtonBar from './components/BurnButtonBar';
+import BurnStatsContainer from './components/BurnStatsContainer';
+import BurnTxTable from './components/BurnTxTable';
+import ChainSelector from './components/ChainSelector';
+import AppToast from './components/AppToast';
 
 const BurnPage = ({ executeBurn, burnAmount, onChangeBurnAmount, txButton, txProgress, statsSupplies, burnTransactions, coinData }) => {
 
