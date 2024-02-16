1ae68c0d5a175c589f288df4118d4b6c461e78a6
Devendra Venkat Mutyala <88088773+devendravenkatm@users.noreply.github.com>
2024-02-16 17:22:15 +0530

    Update BurnPage.tsx

--- after/page.tsx
+++ after/page.tsx
@@ -1,1 +1,33 @@
+import React from 'react';
+import BurnButtonBar from './BurnButtonBar';
+import BurnStatsContainer from './BurnStatsContainer';
+import BurnTxTable from './BurnTxTable';
+import ChainSelector from './ChainSelector';
+import AppToast from './AppToast';
+
+const BurnPage = ({ executeBurn, burnAmount, onChangeBurnAmount, txButton, txProgress, statsSupplies, burnTransactions, coinData }) => {
+
+  return (
+    <div className="burn-page">
+      <div className="top-container">
+        <BurnButtonBar
+          executeBurn={executeBurn}
+          burnAmount={burnAmount}
+          onChangeBurnAmount={onChangeBurnAmount}
+          txButton={txButton}
+          txProgress={txProgress}
+        />
+        <BurnStatsContainer statsSupplies={statsSupplies} />
+      </div>
+
+      <BurnTxTable data={burnTransactions} priceUSD={coinData?.current_price?.usd} />
+
+      <ChainSelector />
+
+      <AppToast />
+    </div>
+  );
+};
+
+export default BurnPage;
 
