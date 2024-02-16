572c885f7e250c2fe472b2346ec035bab4194987
Devendra Venkat Mutyala <88088773+devendravenkatm@users.noreply.github.com>
2024-02-16 17:19:20 +0530

    Create AppToast.tsx

---
+++ after/components/AppToast.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AppToast = () => {
+  return (
+    <div className="app-toast">
+      {/* Render toast messages here */}
+    </div>
+  );
+};
+
+export default AppToast;
